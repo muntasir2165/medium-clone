3554b80449c541ba1fd60d89a47cf120a527c834
Muntasir Biojid <muntasir2165@hotmail.com>
2024-08-17 15:13:56 -0400

    19-Updating auth reducers

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -4,14 +4,38 @@ import { authActions } from './actions';
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
+  isLoading: false,
+  currentUser: undefined,
+  validationErrors: null,
 };
 
 const authFeature = createFeature({
   name: 'auth',
   reducer: createReducer(
     initialState,
-    on(authActions.register, (state) => ({ ...state, isSubmitting: true }))
+    on(authActions.register, (state) => ({
+      ...state,
+      isSubmitting: true,
+      validationErrors: null,
+    })),
+    on(authActions.registerSuccess, (state, action) => ({
+      ...state,
+      isSubmitting: false,
+      currentUser: action.currentUser,
+    })),
+    on(authActions.registerFailure, (state, action) => ({
+      ...state,
+      isSubmitting: false,
+      validationErrors: action.errors,
+    }))
   ),
 });
 
-export const { name: authFeatureKey, reducer: authReducer, selectIsSubmitting } = authFeature;
+export const {
+  name: authFeatureKey,
+  reducer: authReducer,
+  selectIsSubmitting,
+  selectIsLoading,
+  selectCurrentUser,
+  selectValidationErrors,
+} = authFeature;
