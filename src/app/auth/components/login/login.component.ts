a8a159e2f3cfdccfcefbc5774315b16921453422
Muntasir Biojid <muntasir2165@hotmail.com>
2024-08-18 10:40:21 -0400

    23-Login component

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { authActions } from '../../store/actions';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
+import { RouterLink } from '@angular/router';
+import {
+  selectIsSubmitting,
+  selectValidationErrors,
+} from '../../store/reducers';
+import { CommonModule } from '@angular/common';
+import { combineLatest } from 'rxjs';
+import { BackendErrorMessages } from 'src/app/shared/components/backendErrorMessages.component';
+import { LoginRequestInterface } from '../../types/loginRequest.interface';
+
+@Component({
+  selector: 'mc-login',
+  templateUrl: './login.component.html',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    CommonModule,
+    BackendErrorMessages,
+  ],
+})
+export class LoginComponent {
+  form = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  data$ = combineLatest({
+    isSubmitting: this.store.select(selectIsSubmitting),
+    backendErrors: this.store.select(selectValidationErrors),
+  });
+
+  constructor(private fb: FormBuilder, private store: Store) {}
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue());
+    const request: LoginRequestInterface = {
+      user: this.form.getRawValue(),
+    };
+    this.store.dispatch(authActions.login({ request }));
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,16 @@ import { authFeatureKey, authReducer } from './app/auth/store/reducers';
 import { provideHttpClient } from '@angular/common/http';
 import { provideEffects } from '@ngrx/effects';
 import * as authEffects from './app/auth/store/effects';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(appRoutes),
-    provideStore(),
+    provideStore({
+      router: routerReducer
+    }),
+    provideRouterStore(),
     provideState(authFeatureKey, authReducer),
     provideEffects(authEffects),
     provideStoreDevtools({
