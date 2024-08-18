f047d5c42fa9f7471ea0c4316778e191c774ffde
Muntasir Biojid <muntasir2165@hotmail.com>
2024-08-18 12:40:35 -0400

    26-Auth interceptor

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,18 @@ import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { isDevMode } from '@angular/core';
 import { authFeatureKey, authReducer } from './app/auth/store/reducers';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideEffects } from '@ngrx/effects';
 import * as authEffects from './app/auth/store/effects';
 import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import { authInterceptor } from './app/shared/services/authInterceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(appRoutes),
     provideStore({
-      router: routerReducer
+      router: routerReducer,
     }),
     provideRouterStore(),
     provideState(authFeatureKey, authReducer),
