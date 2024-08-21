50d0cfd197465708d2176fc55f73c09eb95dfa19
Muntasir Biojid <muntasir2165@hotmail.com>
2024-08-21 14:39:25 -0400

    37-Adding feed toggler

--- src/app/globalFeed/components/globalFeed/globalFeed.component.ts
+++ src/app/globalFeed/components/globalFeed/globalFeed.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from 'src/app/shared/components/banner/banner.component';
 import { FeedComponent } from 'src/app/shared/components/feed/feed.component';
-import { PopularTagsComponent } from "../../../shared/components/popularTags/popularTags.component";
+import { PopularTagsComponent } from '../../../shared/components/popularTags/popularTags.component';
+import { FeedTogglerComponent } from 'src/app/shared/components/feedToggler/feedToggler.component';
 
 @Component({
   selector: 'mc-global-feed',
   templateUrl: './globalFeed.component.html',
   standalone: true,
-  imports: [FeedComponent, BannerComponent, PopularTagsComponent],
+  imports: [
+    FeedComponent,
+    BannerComponent,
+    PopularTagsComponent,
+    FeedTogglerComponent,
+  ],
 })
 export class GlobalFeedComponent {
   apiUrl = '/articles';
