41a21de770ad300711689babec5a29c1ec741d37
Muntasir Biojid <muntasir2165@hotmail.com>
2024-08-22 00:02:53 -0400

    38-creating other feeds

---
+++ src/app/yourFeed/yourFeed.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { YourFeedComponent } from './components/yourFeed/yourFeed.component';
+
+export const routes: Route[] = [{ path: '', component: YourFeedComponent }];
