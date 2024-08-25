124442ad2e7b2478760bfe2a4ba3baf399339a6a
Muntasir Biojid <muntasir2165@hotmail.com>
2024-08-25 00:36:17 -0400

    44-Creating article store

--- src/app/createArticle/components/createArticle/createArticle.component.ts
+++ src/app/createArticle/components/createArticle/createArticle.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { combineLatest } from 'rxjs';
 import { ArticleFormComponent } from 'src/app/shared/components/articleForm/articleForm.component';
 import { ArticleFormValuesInterface } from 'src/app/shared/components/articleForm/types/articleFormValues.interface';
+import {
+  selectIsSubmitting,
+  selectValidationErrors,
+} from '../../store/reducers';
+import { ArticleRequestInterface } from 'src/app/shared/types/articleRequest.interface';
+import { createArticleActions } from '../../store/actions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'mc-create-article',
   templateUrl: './createArticle.component.html',
   standalone: true,
-  imports: [ArticleFormComponent],
+  imports: [ArticleFormComponent, CommonModule],
 })
 export class CreateArticleComponent {
   initialValues = {
@@ -16,7 +25,17 @@ export class CreateArticleComponent {
     tagList: [],
   };
 
+  data$ = combineLatest({
+    isSubmitting: this.store.select(selectIsSubmitting),
+    backendErrors: this.store.select(selectValidationErrors),
+  });
+
+  constructor(private store: Store) {}
+
   onSubmit(articleFormValues: ArticleFormValuesInterface): void {
-    console.log('onSubmit in create article', articleFormValues);
+    const request: ArticleRequestInterface = {
+      article: articleFormValues,
+    };
+    this.store.dispatch(createArticleActions.createArticle({ request }));
   }
 }
